d112709470beba74729fc36a63af862d963f94a3
metalgeek <oladipolekky@gmail.com>
2021-09-30 14:47:37 +0100

    flight search

--- index.php
+++ index.php
@@ -74,42 +74,12 @@
 				<div class="container">
 					<h3>SEE THE WORLD AT EASE</h3>
 					<p>Flight with Neezerlink...</p>
-					<form>
-						<div class="row no-gutters custom-search-input-2">
-							<div class="col-lg-4">
-								<div class="form-group">
-									<input class="form-control" type="text" placeholder="Hotel, City...">
-									<i class="icon_pin_alt"></i>
-								</div>
-							</div>
-							<div class="col-lg-3">
-								<div class="form-group">
-									<input class="form-control" type="text" name="dates" placeholder="When..">
-									<i class="icon_calendar"></i>
-								</div>
-							</div>
-							<div class="col-lg-3">
-								<div class="panel-dropdown">
-									<a href="#">Guests <span class="qtyTotal">1</span></a>
-									<div class="panel-dropdown-content">
-										<!-- Quantity Buttons -->
-										<div class="qtyButtons">
-											<label>Adults</label>
-											<input type="text" name="qtyInput" value="1">
-										</div>
-										<div class="qtyButtons">
-											<label>Childrens</label>
-											<input type="text" name="qtyInput" value="0">
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="col-lg-2">
-								<input type="submit" class="btn_search" value="Search">
-							</div>
-						</div>
-						<!-- /row -->
-					</form>
+				
+					<section class="flight-search-container">
+        			<div class="container flight-search">
+          			<iframe id='travelstartIframe-32647fb7-728a-4052-8339-e691a7a9f48d' frameBorder='0' scrolling='no' style='margin: 0px; padding: 0px; border: 0px; height: 0px; background-color: #fafafa;'></iframe>
+        			</div>
+      				</section>
 				</div>
 			</div>
 		</div>
